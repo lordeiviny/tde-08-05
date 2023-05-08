3d742decc7f80ba9d6778ba3078fc27204e459a0
lordeiviny <132486172+lordeiviny@users.noreply.github.com>
2023-05-08 18:00:43 -0300

    Update exe02.js

--- exe02.js
+++ exe02.js
@@ -1,11 +1,12 @@
-function somarArray(array) {
-    let soma = 0;
-    for (let i = 0; i < array.length; i++) {
-      soma += array[i];
-    }
-    return soma;
+function somaArray(array) {
+  let soma = 0;
+  for (let i = 0; i < array.length; i++) {
+    soma += array[i];
   }
+  return soma;
+}
 
-const array = [2, 4, 6, 8];
-const resultado = somarArray(array);
+
+const numeros = [2, 4, 6, 10];
+const resultado = somaArray(numeros);
 console.log(`Soma: ${resultado}`);
